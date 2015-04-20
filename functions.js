b32199ebf51b0bf7136d25044dc7453d2efda120
unknown <rpattee@uoregon.edu>
2015-04-19 20:05:26 -0700

    second correct .js

--- functions.js
+++ functions.js
@@ -1,52 +1,69 @@
-"use strict";
+//part A
 
-var tNumsPlus5 = function(tnum) {
- var result = "", digit;
- tnum = String(tnum);
- for(var i = 0; i < tnum.length; ++i) {
-  digit = (Number(tnum[i]) + 5) % 10;
-  console.log(digit);
- }
- return result;
-};
-
-var isArmstrong = function(num) {
- var result = "";
- var digit1 = num.toString()[0];
- var digit2 = num.toString()[1];
- var digit3 = num.toString()[2];
- 
- digit1 = parseInt(digit1);
- digit2 = parseInt(digit2);
- digit3 = parseInt(digit3);
-
- digit1 = Math.pow(digit1, 3);
- digit2 = Math.pow(digit2, 3);
- digit3 = Math.pow(digit3, 3);
-
- var x = digit1 + digit2 +digit3;
-
- if(x === num){
-  return true;
- }
- else{
-  return false;
- }
-};
-
-var allArmstrong3s = function() {
- var result = '' ;
- for(var k = 100; k <= 999 ; ++k)
-  if(isArmstrong(kiss))
-   result = result + k + '';
-  return result ;
-
+function isArmstrong(n){
+	n=String(n);
+	sum =0;
+	for(i=0; i<n.length; i++){
+		sum+=Math.pow(parseInt(n[i]),n.length);
+	}
+	n=parseInt(n);
+	if(n==sum){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
-var formatTelNum = function(n) {
- var num = n.toString();
- var result = '(' + num[0] + num[1] + num[2] + ')' + num[3] + num[4] + num[5] +'-' + num[6] + num[7] + num[8] + num[9];
- console.log(result);
- return result;
+//part B
+function allArmstrongs(){
+	"use strict"
+	var result = "";
+for(var i=1; i<=100000; i++){
+	if(isArmstrong(i))
+		result=result+i+" ";
+	}
+		return result;
+}
 
+//part C
+function allSubstrings(s){
+	var result = "";
+for(var i=0; i<=s.length; i++){
+	for(var j=i+1; j<=s.length;j++){
+		result=result+s.substring(i,j)+", ";
+	}
+}
+return result;
+}
+
+
+//part D
+
+function allSubstrings2(s){
+var result=[];
+for(i=0; i<=s.length; i++){
+	for(j=i+1; j<=s.length;j++){
+		var sub=s.substring(i,j);
+		result.push(sub);
+	}
+}
+	return result;
+}
+
+//part E
+
+function maxWord(s) {
+    var str = s.split(" ");
+    var longest = 0;
+    var word = "";
+    for (var i = 0; i < str.length ; i++) {
+        if (word < str[i].length) {
+            word = str[i].length;
+            longest = str[i];
+        
+        }
+
+    }
+    return longest;
 }
